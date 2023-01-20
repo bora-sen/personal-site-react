d03d8db0c57d5795d8395ec3e077915d8777dba9
bora-sen <bahattinborasen@gmail.com>
2023-01-20 12:16:24 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='bg-gray-800'>
+      <h1 className='text-5xl font-bold text-white'>Hello World!</h1>
+    </main>
   );
 }
-
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      colors:{
+        "primary":"#DDD0C8",
+        "primary-dark":"#323232"
+      }
+    },
+  },
+  plugins: [],
+}
