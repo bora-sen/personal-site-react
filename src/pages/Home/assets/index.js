4f6e59a771c6ca087005b7b7b12478431b9d6ae0
bora-sen <bahattinborasen@gmail.com>
2023-02-11 21:34:01 +0300

    refactor: Image Directories Changed

--- src/pages/Home/assets/index.js
+++ src/pages/Home/assets/index.js
@@ -1,7 +1,4 @@
-export {default as HomeHeroIMG} from './home-hero.gif'
 export {default as GithubProfileIMG} from './github-preview.png'
 export {default as LinkedInProfileIMG} from './linkedin.png'
 export {default as InstagramProfile} from './instagram.png'
-export {default as WorksHermor} from './works-hermor.png';
-export {default as WorksDraw} from './works-drawndpaint.jpg';
 export {default as ProfileImage} from './pp.jpg'
