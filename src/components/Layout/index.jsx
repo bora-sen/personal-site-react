838b2fd454aa9dffd9bef0ad12ca473f06d1d796
bora-sen <bahattinborasen@gmail.com>
2023-02-14 20:41:17 +0300

    changed

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -3,14 +3,14 @@ import Header from '../Header'
 import Footer from '../Footer'
 
 function Layout({children}) {
-  return (
-    <main className='font-poppins dark:text-primary sm:w-[45rem] md:w-[50rem] mx-auto'>
-        <Header />
-            {children}
-        <Footer />
-        
-    </main>
-  )
+    return (
+      <main className='font-poppins dark:text-primary sm:w-[45rem] md:w-[50rem] mx-auto'>
+          <Header />
+              {children}
+          <Footer />
+          
+      </main>
+    )
 }
 
 export default Layout
