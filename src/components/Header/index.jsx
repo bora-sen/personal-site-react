c29e836f1a0cf42ae7d2107a3a88deaf94595c90
bora-sen <bahattinborasen@gmail.com>
2023-02-26 11:41:46 +0300

    feat: changed header icon

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,16 +3,11 @@ import ThemeSwitchButton from '../ThemeSwitchButton'
 
 function Header() {
   return (
-    <section className='sticky flex-col sm:flex-row gap-2 sm:gap-0 top-0 z-10 font-poppins flex justify-between items-center px-3 py-2 h-auto mb-4 shadow-xl backdrop-blur-sm'>
+    <section className='sticky flex-col sm:flex-row gap-4 sm:gap-0 top-0 z-10 font-poppins flex justify-between items-center px-3 py-2 h-auto mb-4 shadow-xl backdrop-blur-sm'>
       
       <div className='group flex hover:cursor-default'>
-        <svg className='w-6 fill-transparent dark:fill-primary-dark dark:stroke-primary -rotate-12 mr-1 transition-all group-hover:rotate-0' stroke="#323232" strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path d="M19.412 7.214c.729 5.184-3.2 9.528-6.167 9.946-2.968.418-7.945-2.695-8.672-7.86A7.492 7.492 0 0 1 19.41 7.214h.002Z" />
-          <path d="M11.065 14.443c-1.539-.609-3.144-2.02-3.856-4.005" />
-          <path d="M17.236 23.238c-1.539-.61-2.967-1.878-3.68-3.863" />
-          <path d="m12.502 17.263-.43 2.326 2.968-.417-1.054-2.117-1.484.208Z" />
-        </svg>
-        <h1 className='font-bold'>Bahattin Bora Şen</h1>
+      <svg className='stroke-primary-dark dark:stroke-primary w-8 h-8 sm:w-6 sm:h-6 mr-2 sm:-rotate-12 sm:group-hover:rotate-0 transition-all' fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M20.25 2.25H3.75A2.25 2.25 0 0 0 1.5 4.5v15a2.25 2.25 0 0 0 2.25 2.25h16.5a2.25 2.25 0 0 0 2.25-2.25v-15a2.25 2.25 0 0 0-2.25-2.25Z" /><path d="m4.5 5.25 3.75 3-3.75 3" /><path d="M9 11.25h3" /></svg>
+        <h1 className='font-bold text-2xl sm:text-lg'>Bahattin Bora Şen</h1>
       </div>
 
       <div className='flex items-center justify-center gap-8'>
