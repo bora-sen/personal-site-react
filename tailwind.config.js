150abe7943c4c60f156c510a47cb55a14e592939
bora-sen <bahattinborasen@gmail.com>
2023-03-28 16:18:55 +0300

    +update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content : ["./src/**/*.{html,js,jsx}"],
-  darkMode: 'class',
+  content: ["./src/**/*.{html,js,jsx}"],
+  darkMode: "class",
   theme: {
     extend: {
-      colors:{
-        "primary":"#fdfcfb",
-        "primary-dark":"#010101",
-        "paragraph":"#594a4e",
-        "accentOrange":"#b6152a",
-        "accentGreen":"#188a0e",
-        "accentAqua":"#3958a2",
-        "accentRed":"#881a45",
+      colors: {
+        primary: "#fdfcfb",
+        "primary-dark": "#010101",
+        paragraph: "#242ac2",
+        accentOrange: "#e81e6b",
+        accentGreen: "#188a0e",
+        accentAqua: "#242ac2",
+        accentRed: "#881a45",
       },
-      fontFamily:{
-        "poppins":['Poppins',"sans-serif"],
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
       },
     },
   },
