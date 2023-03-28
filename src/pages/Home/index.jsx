d01d232a3415fa71edec2d6e3c9f9d626a73c8d3
bora-sen <bahattinborasen@gmail.com>
2023-03-28 16:18:40 +0300

    +added Model Loader

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,16 @@
-import React from 'react'
-import {Layout} from '../../components'
-import { ILoveTo, OnTheWeb, RotateHouse, WhoAmI, Works } from './sections'
+import React from "react"
+import { Layout } from "../../components"
+import { ILoveTo, OnTheWeb, RotateHouse, WhoAmI, Works } from "./sections"
 
 function Home() {
-
-
   return (
-      <Layout>
-        <RotateHouse />
-          <WhoAmI />
-          <ILoveTo />
-          <OnTheWeb />
-          <Works />
-      </Layout>
+    <Layout>
+      <RotateHouse />
+      <WhoAmI />
+      <ILoveTo />
+      <OnTheWeb />
+      <Works />
+    </Layout>
   )
 }
 
--- src/pages/Home/sections/RotateDiorama.jsx
+++ src/pages/Home/sections/RotateDiorama.jsx
@@ -1,60 +1,45 @@
-import { Html, OrbitControls, PerspectiveCamera, useProgress } from '@react-three/drei'
-import React, { Suspense, useContext } from 'react'
-import { Canvas } from 'react-three-fiber'
-import { ThemeContext } from '../../../context/ThemeContext'
-import RotatingDiorama from '../model/RotatingDiorama'
-
-
-function LoadingSpinner(){
-  const {progress} = useProgress();
-  return (
-    <Html center>
-      <div className='text-2xl font-extrabold font-poppins flex flex-col items-center justify-center'>
-        <h2>Loading..</h2>
-        <h3>{progress}%</h3>
-      </div>
-    </Html>
-  )
-}
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei"
+import React, { Suspense, useContext } from "react"
+import { Canvas } from "react-three-fiber"
+import { ThemeContext } from "../../../context/ThemeContext"
+import LoadedModel from "../../../LoadedModel"
 
 function RotateHouse() {
-  let theme = useContext(ThemeContext).Theme;
+  let theme = useContext(ThemeContext).Theme
 
   function handleLights() {
-    if(theme === "light"){
+    if (theme === "light") {
       return (
         <>
           <ambientLight intensity={0.7} />
-          <pointLight castShadow position={[0,0,4]} intensity={0.1} />
-          <pointLight castShadow position={[1,0,0]} intensity={1} color="#0000f3" />
-          <pointLight castShadow position={[0,0,-1]} intensity={1} color="#00f300" />
+          <pointLight castShadow position={[0, 0, 4]} intensity={0.1} />
+          <pointLight castShadow position={[1, 0, 0]} intensity={1} color="#0000f3" />
+          <pointLight castShadow position={[0, 0, -1]} intensity={1} color="#00f300" />
         </>
       )
-      
-    }
-    else if(theme === "dark"){
-        return (
-          <>
-          <ambientLight intensity={0.1}/>
-          <pointLight castShadow position={[0,0,4]} intensity={0.1} />
-          <pointLight castShadow position={[1,0,0]} intensity={0.5} color="#0000f3" />
-          <pointLight castShadow position={[0,0,-1]} intensity={0.4} color="#00f300" />
+    } else if (theme === "dark") {
+      return (
+        <>
+          <ambientLight intensity={0.1} />
+          <pointLight castShadow position={[0, 0, 4]} intensity={0.1} />
+          <pointLight castShadow position={[1, 0, 0]} intensity={0.5} color="#0000f3" />
+          <pointLight castShadow position={[0, 0, -1]} intensity={0.4} color="#00f300" />
         </>
       )
     }
   }
   return (
     <>
-    <div className='w-full h-[30rem]'>
+      <div className="w-full h-[30rem]">
         <Canvas shadows="variance">
-          <PerspectiveCamera makeDefault position={[0,1.5,6.5]} fov={45} />
-        <OrbitControls autoRotate autoRotateSpeed={0.12} enableZoom={false} />
-            <Suspense fallback={<LoadingSpinner />}>
-              {handleLights()}
-            <RotatingDiorama />
-            </Suspense>
+          <PerspectiveCamera makeDefault position={[0, 1.5, 6.5]} fov={45} />
+          <OrbitControls autoRotate autoRotateSpeed={0.12} enableZoom={false} />
+          {handleLights()}
+          <Suspense fallback={null}>
+            <LoadedModel />
+          </Suspense>
         </Canvas>
-    </div>
+      </div>
     </>
   )
 }
